cb058854364030d7d29239c49e6cdd8b777a33b8
Jettfeng <1130034920@qq.com>
2018-01-25 22:16:56 +0800

    提交

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import App from './App'
+// import router from './router'
+
+// 引入公共样式
+import 'common/stylus/index.styl'
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
